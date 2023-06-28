f4cdaa028c9dd8c5dc541bab91d5a6ecf3256cf1
Adrien Legros <alegros@redhat.com>
2023-06-28 20:17:22 +0200

    organize dir

--- inference/test/predict.sh
+++
@@ -1,4 +0,0 @@
-ENDPOINT=$(oc -n mnist get route mnist -ojsonpath='{.status.ingress[0].host}')
-curl --insecure --silent -X POST \\ 
--d @gitops/test/body.json \\
-https://${ENDPOINT}/v2/models/mnist/infer | jq
