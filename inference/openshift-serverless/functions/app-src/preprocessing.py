bb7248c3eed36a7b9933c8c356e854647b6e43cc
Adrien Legros <alegros@redhat.com>
2023-06-28 20:13:07 +0200

    handle png from minio with dark background and write to kafka (should be change later)

--- inference/openshift-serverless/functions/app-src/preprocessing.py
+++ inference/openshift-serverless/functions/app-src/preprocessing.py
@@ -8,12 +8,22 @@ from PIL import Image
 
 MODEL_VERSION = os.environ.get("MODEL_VERSION") or "v2"
 
-def process_data(img_string):
-    img_str = re.search(r'base64,(.*)', img_string).group(1)
-    image_bytes = io.BytesIO(base64.b64decode(img_str))
-    im = Image.open(image_bytes)
-    arr = np.array(im)[:,:,0:1]
-    arr = (255 - arr) / 255
+def process_data(img_string, provider):
+    if provider == "webapp":
+        img_str = re.search(r'base64,(.*)', img_string).group(1)
+        image_bytes = io.BytesIO(base64.b64decode(img_str))
+        im = Image.open(image_bytes)
+        arr = np.array(im)[:,:,0:1]
+        arr = (255 - arr) / 255
+    elif provider == "kafka":
+        img_str = img_string
+        image_bytes = io.BytesIO(base64.b64decode(img_str))
+        im = Image.open(image_bytes)
+        arr = np.array(im)[:,:]
+        arr = 1 - ((255 - arr) / 255)
+    else:
+        raise Exception(f"Provider {provider} not implemented.")
+    print(arr.shape)
     res = arr.reshape(1,-1).tolist()
     return res
 
