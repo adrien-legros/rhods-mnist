ce3dc9e665a8fef5719f3164762bc6a1ed3c5574
Adrien Legros <alegros@redhat.com>
2023-07-13 10:32:01 +0200

    variabilize

--- test/batch/push-minio.sh
+++ test/batch/push-minio.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mc alias set remote "https://minio-mnist.apps.cluster-l7lrt.l7lrt.sandbox2852.opentlc.com/" "minio" "minio123"
+# Arg1: minio endpoint
+mc alias set remote $1 "minio" "minio123"
 for img in $(ls ./images | shuf);
 do
     mc cp ./images/$img remote/images/$img
