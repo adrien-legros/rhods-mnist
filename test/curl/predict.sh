52445f1f9c02d42796b0d425256a2d4fd29d400a
Adrien Legros <alegros@redhat.com>
2023-10-05 15:40:45 +0200

    rhods 1.33

--- test/curl/predict.sh
+++ test/curl/predict.sh
@@ -1,4 +1,2 @@
 ENDPOINT=$(oc -n mnist get route mnist -ojsonpath='{.status.ingress[0].host}')
-curl --insecure --silent -X POST \\ 
--d @gitops/test/body.json \\
-https://${ENDPOINT}/v2/models/mnist/infer | jq
+curl --insecure --silent -X POST -d @body.json https://${ENDPOINT}/v2/models/mnist/infer | jq
